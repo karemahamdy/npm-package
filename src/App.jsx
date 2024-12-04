d5b2abb2a70193fda601f14bd259a6c3b8298e17
Karema <karemahamdy51@gmail.com>
2024-12-05 01:29:24 +0200

    ..

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Navigate,
+// } from "react-router-dom";
+// import './App.css'
+
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element:
+//       <Applayout />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Navigate replace to="" />,
+//       },
+//       {
+//         path: "",
+//         element: </>,
+//       },
+//       {
+//         path: "",
+//         element: </>,
+//       },
+//       {
+//         path: "",
+//         element: </>,
+//       },
+//       {
+//         path: "",
+//         element: </>,
+//       },
+//       {
+//         path: "",
+//         element: </>,
+//       },
+//       {
+//         path: "",
+//         element: </>,
+//       },
+//       {
+//         path: "",
+//         element: </>,
+//       },
+//       {
+//         path: "",
+//         element: </>,
+//       },
+//     ],
+//   },
+//   {
+//     path: "login",
+//     element: <Login />,
+//   },
+//   {
+//     path: "*",
+//     element: <PageNotFound />,
+//   },
+// ]);
+
+function App() {
+
+  return (
+    <>
+    
+        {/* <RouterProvider router={router} /> */}
+    <h1 className="bg-orange-500">Karema Hamdy</h1>
+      
+    </>
+  )
+}
+
+export default App
